36af3ed7456c89133ed6f91025c25feae983d44b
JCY <1232>
2021-03-06 17:32:17 +0800

    sadf

--- learn1/app1/employee/urls.py
+++ learn1/app1/employee/urls.py
@@ -13,13 +13,13 @@ urlpatterns = [
     path('edit_dep_old/<int:dep_id>/', edit_dep_old),
     path('del_dep_old/<int:dep_id>/', del_dep_old),
 
-    # path('list_group_old', list_group_old),
-    # path('add_group_old', add_group_old),
-    # path('edit_group_old/<int:group_id>', edit_group_old),
-    # path('del_group_old/<int:group_id>', del_group_old),
+    path('list_group_old/', list_group_old),
+    path('add_group_old/', add_group_old),
+    path('edit_group_old/<int:group_id>/', edit_group_old),
+    path('del_group_old/<int:group_id>/', del_group_old),
     #
-    # path('list_employeeinfo_old', list_employeeinfo_old),
-    # path('add_employeeinfo_old', add_employeeinfo_old),
-    # path('edit_employeeinfo_old/<int:info_id>', edit_employeeinfo_old),
-    # path('del_employeeinfo_old/<int:info_id>', del_employeeinfo_old),
+    path('list_employeeinfo_old/', list_employeeinfo_old),
+    path('add_employeeinfo_old/', add_employeeinfo_old),
+    path('edit_employeeinfo_old/<int:info_id>/', edit_employeeinfo_old),
+    path('del_employeeinfo_old/<int:info_id>/', del_employeeinfo_old),
 ]
--- learn1/app1/employee/views.py
+++ learn1/app1/employee/views.py
@@ -54,3 +54,83 @@ def edit_dep_old(request, dep_id):
     else:
         dep_object = department.objects.get(id=dep_id)
         return render(request, 'test_orm_old/edit_dep_old.html', {'department':dep_object})
+
+
+###########################################
+def list_group_old(request):
+    group_list = group.objects.all()
+    return render(request, 'test_orm_old/list_group_old.html', {'group_list':group_list})
+
+def add_group_old(request):
+    if request.method == 'POST':
+        group_name = request.POST.get('group_name')
+        group_script = request.POST.get('group_script')
+        if group_name.strip() == '':
+            return render(request, 'test_orm_old/add_group_old.html', {'error_info':'团体名称不能为空'})
+        try:
+            group.objects.create(group_name=group_name, group_script=group_script)
+            return redirect('/test_orm_old/list_group_old/')
+        except Exception as e:
+            return render(request, 'test_orm_old/add_group_old.html', {'error_info':'输入团体名称重复或者信息有错误!'})
+        finally:
+            pass
+    return render(request, 'test_orm_old/add_group_old.html')
+
+def del_group_old(request, group_id):
+    group_object = group.objects.get(id=group_id)
+    group_object.delete()
+    return redirect('/test_orm_old/list_group_old/')
+
+
+def edit_group_old(request, group_id):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        group_name = request.POST.get('group_name')
+        group_script = request.POST.get('group_script')
+        group_object = group.objects.get(id=id)
+        group_object.group_name = group_name
+        group_object.group_script = group_script
+        group_object.save()
+        return redirect('/test_orm_old/list_group_old/')
+    else:
+        group_object = group.objects.get(id=group_id)
+        return render(request, 'test_orm_old/edit_group_old.html', {'group':group_object})
+
+########################################################
+def list_employeeinfo_old(request):
+    info_list = employeeinfo.objects.all()
+    return render(request, 'test_orm_old/list_employeeinfo.html',{'info_list':info_list})
+
+def add_employeeinfo_old(request):
+    if request.method == 'POST':
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        if phone.strip() == '':
+            return render(request, 'test_orm_old/add_employeeinfo.html',{'error_info':'电话号码不能为空'})
+        try:
+            employeeinfo.objects.create(phone=phone, address=address)
+            return redirect('/test_orm_old/list_employeeinfo_old/')
+        except Exception as e:
+            return render(request, 'test_orm_old/add_employeeinfo.html', {'error_info':'信息有错误'})
+        finally:
+            pass
+    return render(request, 'test_orm_old/add_employeeinfo.html')
+
+def del_employeeinfo_old(request, info_id):
+    info_object = employeeinfo.objects.get(id=info_id)
+    info_object.delete()
+    return redirect('/test_orm_old/list_employeeinfo_old')
+
+def edit_employeeinfo_old(request, info_id):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        info_object = employeeinfo.objects.get(id=id)
+        info_object.phone = phone
+        info_object.address = address
+        info_object.save()
+        return redirect('/test_orm_old/list_employeeinfo_old/')
+    else:
+        info_object = employeeinfo.objects.get(id=info_id)
+        return render(request, 'test_orm_old/edit_employeeinfo.html', {'info':info_object})
